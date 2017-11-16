3c776d5fbc5c52c0a431feda95594276ee4e31d4
Michael Thane <Asy-dk@live.dk>
2017-11-16 23:00:38 +0100

    Ball scaling, animation and loop.
    Added:
    - Sprite scaling
    - Function to animate the sprite
    - Function loop to make ball bounce between two restrictions.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ renderer.view.style.border = "1px dashed black";
 
 document.body.appendChild(renderer.view);
 var stage = new PIXI.Container();
+var speed = 3;
 
 PIXI.loader.add("ball.png").load(setup);
 
@@ -10,6 +11,21 @@ function setup() {
   ball = new PIXI.Sprite(
     PIXI.loader.resources["ball.png"].texture
   );
+  ball.scale.x = 0.2;
+  ball.scale.y = 0.2;
   stage.addChild(ball);
+  animate();
   renderer.render(stage);
+}
+function animate() {
+  requestAnimationFrame(animate);
+  loop();
+  renderer.render(stage);
+}
+
+function loop(){
+  if ( ball.y > 400 || ball.y < 0){
+    speed = speed * -1;
+  }
+  ball.y = ball.y += speed;
 }
