3385dd47c955916cd0f435dd506f63a285951762
Michael Thane <Asy-dk@live.dk>
2017-11-17 01:11:21 +0100

    Implementing Cubic Bezier
    Implemented Cubic Bezier formula to create an effect on the ball's bounce like a CSS ease effect.
    
    Removed speed and implemented time and acceleration

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ renderer.view.style.border = "1px dashed black";
 
 document.body.appendChild(renderer.view);
 var stage = new PIXI.Container();
-var speed = 3;
+var ball;
+var start_y_position = 400 // canvas(500) - sprite size (100)
+var end_y_position = 0;
+var time = 0.99; //Time is between 1 and 0
+var acceleration = 0.01; //Acceleration is between 1 and 0
 
 PIXI.loader.add("ball.png").load(setup);
 
@@ -15,7 +19,6 @@ function setup() {
   ball.scale.y = 0.2;
   stage.addChild(ball);
   animate();
-  renderer.render(stage);
 }
 function animate() {
   requestAnimationFrame(animate);
@@ -23,9 +26,22 @@ function animate() {
   renderer.render(stage);
 }
 
+//Loop function to determine the position of the sprite
 function loop(){
-  if ( ball.y > 400 || ball.y < 0){
-    speed = speed * -1;
+  time += acceleration;
+  if (time >= 1){
+    acceleration *= -1; //Implementation to swap direction for hitting the boundries
   }
-  ball.y = ball.y += speed;
+else if(time <= 0)
+{
+   acceleration *= -1;
+}
+  console.log(time);
+  ball.y = start_y_position + (end_y_position - start_y_position) * cubicBezier(time, 0, 1, 1, 1); // use cubic bezier to calculate the current y position
+
+}
+
+//This formula was found on SO but is the explicit Cubic Bezier formula found on https://en.wikipedia.org/wiki/B%C3%A9zier_curve - Section Cubic Bézier curves. (Ease effect)
+function cubicBezier(t, p0, p1, p2, p3) {
+    return (1-t)**3*p0+3*(1-t)**2*t*p1+3*(1-t)*t**2*p2+t**3*p3;
 }
